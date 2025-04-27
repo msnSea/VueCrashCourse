7f8b3c7cca57c1bd0412e371891570ffa78baf0a
Mohsen Siaghi <MohsenSiaghi@gmail.com>
2025-04-27 23:05:05 +0330

    App.vue: Make simple fixed UI with HTML CSS

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
-<template>
-  <H1>Hello World!</H1>
-</template>
+<main>
+  <template>
+    <H1>Hello World!</H1>
+  </template>
+</main>
+
+<style scoped>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+}
+</style>
