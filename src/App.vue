75ed4aa73308c06cf8bb19672046b99c55738c29
Mohsen Siaghi <MohsenSiaghi@gmail.com>
2025-05-05 14:39:26 +0330

    Creating Add button and new task textbox in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script lang="ts" setup>
   import { ref } from 'vue';
-  const message = ref("Hello World!!")
+  const message = ref("TASKs APP")
+  const tempValueHolder = ref("");
+  function formSubmitted() {
+    console.log(tempValueHolder.value);
+  }
 </script>
 
 <template>
   <main>
-      <H1>{{message}}</H1>
+    <h1>{{message}}</h1>
+    <form @submit.prevent="formSubmitted">
+        <label>
+          new TASK
+          <input v-model="tempValueHolder" name="tempValueHolder">
+        </label>
+        <div class="button-container">
+          <button>Add</button>
+        </div>
+      </form>
     </main>
 </template>
 
@@ -13,5 +26,9 @@
 main {
   max-width: 800px;
   margin: 1rem auto;
+}
+.button-container {
+  display: flex;
+  justify-content: end;
 }
 </style>
