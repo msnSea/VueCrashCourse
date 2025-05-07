11ba6ad2cf2b6569b78f1cbd0d065197d1208cf8
Mohsen Siaghi <MohsenSiaghi@gmail.com>
2025-05-07 19:23:41 +0330

    create/extract TaskForm.vue from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <script lang="ts" setup>
   import { ref } from 'vue';
+  import TaskForm from './components/TaskForm.vue';
   const message = ref("TASKs APP")
-  const tempValueHolder = ref("");
-  function formSubmitted() {
-    console.log(tempValueHolder.value);
-  }
 </script>
 
 <template>
   <main>
     <h1>{{message}}</h1>
-    <form @submit.prevent="formSubmitted">
-        <label>
-          new TASK
-          <input v-model="tempValueHolder" name="tempValueHolder">
-        </label>
-        <div class="button-container">
-          <button>Add</button>
-        </div>
-      </form>
+    <TaskForm />
     </main>
 </template>
 
-<style scoped>
+<style>
 main {
   max-width: 800px;
   margin: 1rem auto;
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+  import { ref } from 'vue';
+
+  const newTask = ref("");
+  
+  function formSubmitted() {
+    console.log(newTask.value);
+  }
+</script>
+
+<template>
+    <form @submit.prevent="formSubmitted">
+        <label>
+        new TASK
+        <input v-model="newTask" name="newTask">
+        </label>
+        <div class="button-container">
+        <button>Add</button>
+        </div>
+  </form>
+</template>
+
