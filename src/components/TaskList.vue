6379ce43ec0771a1ae48cd4e4d9700528995388d
Mohsen Siaghi <MohsenSiaghi@gmail.com>
2025-06-06 07:03:53 +0330

    Create Task list component (core feature: listing Items) + define Task item data structure

---
+++ src/components/TaskList.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import type { Task } from './types';
+
+    const props = defineProps<{
+        tasks: Task[]
+    }>();
+</script>
+
+<template>
+    <article v-for="task in props.tasks" :key="task.id">
+        {{ task.title }}
+    </article>
+</template>
